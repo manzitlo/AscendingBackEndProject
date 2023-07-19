aa12abd9ae43874baf0b43330346a50630d4639a
Wenzhe Luo <100318920+manzitlo@users.noreply.github.com>
2023-07-18 22:30:54 -0400

    updated DB migration

---
+++ src/main/resources/db/migration/V1.2__create_user_role_table.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+    id          BIGSERIAL NOT NULL,
+    name        VARCHAR(30) not null unique,
+    password    VARCHAR(64),
+    secret_key  varchar(512),
+    first_name  VARCHAR(30),
+    last_name   VARCHAR(30),
+    email       VARCHAR(50) not null unique
+);
+
+ALTER TABLE users ADD CONSTRAINT user_pk PRIMARY KEY ( id );
+
+CREATE TABLE roles (
+    id                       BIGSERIAL NOT NULL,
+    name                     VARCHAR(30) not null unique,
+    allowed_resource         VARCHAR(200),
+    allowed_read             BOOLEAN not null default false,
+    allowed_create           BOOLEAN not null default false,
+    allowed_update           BOOLEAN not null default false,
+    allowed_delete           BOOLEAN not null default false
+);
+
+ALTER TABLE roles ADD CONSTRAINT roles_pk PRIMARY KEY ( id );
+
+CREATE TABLE users_roles (
+    user_id     BIGINT NOT NULL,
+    role_id     BIGINT NOT NULL
+);
+
+ALTER TABLE users_roles
+    ADD CONSTRAINT users_fk FOREIGN KEY ( user_id )
+        REFERENCES users ( id );
+
+ALTER TABLE users_roles
+    ADD CONSTRAINT roles_fk FOREIGN KEY ( role_id )
+        REFERENCES roles ( id );
