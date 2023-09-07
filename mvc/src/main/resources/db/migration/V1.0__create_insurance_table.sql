d99a5f438f2467f06985fe0ddd0112c7bf0f0ba2
Wenzhe Luo <100318920+manzitlo@users.noreply.github.com>
2023-09-06 22:00:00 -0400

    added new version dependencies

--- mvc/src/main/resources/db/migration/V1.0__create_insurance_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE insurances(
-                   id					BIGSERIAL NOT NULL,
-                   company_name		    VARCHAR(20) not null,
-                   sepcifications		VARCHAR(30) not null
-);
-
-ALTER TABLE insurances ADD CONSTRAINT insurance_pk PRIMARY KEY (id);
